134b8e5afc0bc61581b35214631475eb6c361b64
orkngkc <orkungokce2@gmail.com>
2024-11-13 13:50:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {Routes, Route} from "react-router-dom";
+import Topbar from "./scenes/global/topbar";
+
+//import { Dashboard } from ".scenes/Dashboard";
+import SideNavbar from "./scenes/global/sidenavbar"
+
+/* import Team from "./scenes/team"
+import Invoices from "./scenes/invoices"
+import Contacts from "./scenes/contacts"
+import Bar from "./scenes/bar"
+import Form from "./scenes/form"
+import Line from "./scenes/line"
+import Pie from "./scenes/pie"
+import FAQ from "./scenes/faq"
+import Geography from "./scenes/geograhy"
+import Calendar from "./scenes/calendar" */
 
 
 function App() {
+  const [theme, colorMode] = useMode();
+
+
   return (
-    <div className="app">
-      
-    </div>
+  
+    <ColorModeContext.Provider value = {colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SideNavbar/>
+          <main className = "content">
+            <Topbar />
+             <Routes>
+              {/* <Route path = "/" element= {<Dashboard/>} />
+              <Route path = "/team" element= {<Team/>} />
+              <Route path = "/contacts" element= {<Contacts/>} />
+              <Route path = "/invoices" element= {<Invoices/>} />
+              <Route path = "/form" element= {<Form/>} />
+              <Route path = "/bar" element= {<Bar/>} />
+              <Route path = "/pie" element= {<Pie/>} />
+              <Route path = "/line" element= {<Line/>} />
+              <Route path = "/geography" element= {<Geography/>} />
+              <Route path = "/calendar" element= {<Calendar/>} />
+              <Route path = "/faq" element= {<FAQ/>} /> */}
+             </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+          
+    </ColorModeContext.Provider>
+    
   );
 }
 
